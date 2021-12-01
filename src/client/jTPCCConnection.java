3078c55dddf1e397a82cae15a1c3bd4d1ec17007
guoaomen <guoaomen@actionsky.com>
2021-12-01 15:31:56 +0800

    add limit 1

--- src/client/jTPCCConnection.java
+++ src/client/jTPCCConnection.java
@@ -244,7 +244,7 @@ public class jTPCCConnection {
                 "SELECT no_o_id " +
                         "    FROM bmsql_new_order " +
                         "    WHERE no_w_id = ? AND no_d_id = ? " +
-                        "    ORDER BY no_o_id ASC");
+                        "    ORDER BY no_o_id ASC LIMIT 1");
         stmtDeliveryBGDeleteOldestNewOrder = dbConn.prepareStatement(
                 "DELETE FROM bmsql_new_order " +
                         "    WHERE no_w_id = ? AND no_d_id = ? AND no_o_id = ?");
